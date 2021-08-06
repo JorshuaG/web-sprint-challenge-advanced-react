83242fab6ebc86def26a4403bd69dc01b7d22978
JorshuaG <joshbgearheart@gmail.com>
2021-08-06 00:35:42 -1000

    added custom hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,30 @@
 // write your custom hook here to control your checkout form
+import React from "react";
+import { useState } from "react";
+
+const initialValue = {
+  firstName: "",
+  lastName: "",
+  address: "",
+  city: "",
+  state: "",
+  zip: "",
+};
+
+function useForm(props) {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  return [
+    showSuccessMessage,
+    setShowSuccessMessage,
+    values,
+    setValues,
+    handleChanges,
+  ];
+}
+
+export default useForm;
